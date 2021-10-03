5912dd5a2be958a7e76901d6107d1d9b492ab911
Gurtej736l <gurtej154@gmail.com>
2021-10-03 15:18:56 +1030

    enabling js elements

---
+++ script.js
@@ -0,0 +1,10 @@
+// creating functioning buttons witin the quiz
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const exit_btn = document.querySelector(".buttons .quit");
+const continue_btn = document.querySelector("buttons .continue");
+
+// when the play button is clicked, the following function occurs
+start_btn.onclick = ()=> {
+    info_box.classList.add("");
+}
