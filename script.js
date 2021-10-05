5462c4e9b5f26ef18fd0a48e3c11ab30206c45a4
Gurtej736l <gurtej154@gmail.com>
2021-10-05 16:23:42 +1030

    fixing minor errors with js functions in questions

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const continue_btn = document.querySelector(".continue");
 const quiz_box = document.querySelector(".quiz_box")
 
 // when the play button is clicked, the following function occurs
-start_btn.onclick = ()=> {
+start_btn.onclick = () => {
     // displays the information section
     info_box.classList.add("activeInfo");
 }
 
 // when the exit quizify button is clicked, the following function occurs
-exit_btn.onclick = ()=> {
+exit_btn.onclick = () => {
     //hides the information section 
     info_box.classList.remove("activeInfo");
 }
 
 // when the continue quizify button is clicked, the following function occurs
-continue_btn.onclick = ()=> {
+continue_btn.onclick = () => {
     //hides the information section 
     info_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
@@ -27,70 +27,90 @@ continue_btn.onclick = ()=> {
 
 //When a probable answer is clicked, a alert is given on weather the answer is correct or incorrect
 
-var letQuestions = [{
+var questions = [
+    {
+
+        numb: 1,
         question: "What are the JavaScript data types?",
-        answers: {
-            a:"Number",
-            b:"String",
-            c:"Boolean",
-            d:"Object",
-            e:"Undefined",
-            f:"All the above"
-        }, 
-        correctAnswer: "All the above"
-    }, 
-    {
-        question: "The condition in an if or else statement is enclosed within?",
-        answers: {
-            a:"Quotes",
-            b:"Curly brackets",
-            c:"Parentheses",
-            d:"Square brackets"
-        },
-        correctAnswer: "Parentheses"
+        answer: "All the above",
+        options: [
+            "Number",
+            "String",
+            "Boolean",
+            "Object",
+            "Undefined",
+            "All the above"
+        ]
     },
     {
+        numb: 2,
+        question: "The condition in an if or else ststement is enclosed within?",
+        answer: "Parentheses",
+        options: [
+            "Quotes",
+            "Curly brackets",
+            "Parentheses",
+            "Square brackets",
+            "Parenthese brackets",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 3,
         question: "What tag does JavaScript begin with?",
-        answers: {
-            a:"Script tag",
-            b:"JavaScript tag",
-            c:"Language tag",
-            d:"None of the above"
-        },
-        correctAnswer: "Script tag"
-    },
-    { 
-        question: "JavaScript is a ______ programming language?",
-        answers: {
-            a:"Client",
-            b:"Serverside",
-            c:"Java",
-            d:"None"
-        },
-        correctAnswer: "Server-side"
+        answer: "Script tag",
+        options: [
+            "Script tag",
+            "JavaScript tag",
+            "Language tag",
+            "HTML tag",
+            "CSS tag",
+            "None of the above"
+
+        ]
     },
     {
-        question: "Which of the following will write the message 'Hello World!' in an alert box?",
-        answers: {
-            a:"alert('Hello World!');",
-            b:"alert(HelloWorld!);",
-            c:"msgAlert('Hello World!');",
-            d:"alert('Hello World!');"
-        },
-        correctAnswer: "alert('Hello World');"
-    }]
+        numb: 4,
+        question: "JavaScript is a _________ programming language?",
+        answer: "Server-side",
+        options: [
+            "Client-side",
+            "Server-side",
+            "Java",
+            "Object oriented",
+            "Interative",
+            "All the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is an array?",
+        answer: "A variable that is used to store different elements",
+        options: [
+            "Single variable",
+            "A function",
+            "A variable that is used to store different elements",
+            "A declaration element",
+            "Referencing element",
+            "All the above"
+        ]
+    },
+];
 
-    let que_count = 0;
+let que_count = 0;
 
-    // Function to allow questions to display in the input screen
-    function showQuestions(index){
-        const que_text = document.querySelector(".que_text");
-        const option_list = document.querySelector(".option_list");
-        let que_tag = '<span>'+ letQuestions[index].question +'</span>';
-        let option_tag = '<div clas ="option">'+ letQuestions[index].answers[0] +'<span></span></div>'
-        + '<div class="option"><span></span></div>'
-        + '<div class="option"><span></span></div>'
-        + '<div class="option"><span></span></div>';
-        que_text.innerHTML = que_tag;
-        option_list.innerHTML = option_tag;
-    }
+// Function to allow questions to display in the input screen
+function showQuestions(index) {
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].question +'</span>';
+    let option_tag = '<div class ="option">'+ questions[index].options[0] +'<span></span></div>'
+        + '<div class="option">' + questions[index].options[1] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[2] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[3] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[4] + '<span></span></div>'
+        + '<div class="option">' + questions[index].options[5] + '<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+
+}
