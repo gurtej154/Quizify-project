36a7b594e1e0c284fca926ff56555a010efaa018
Gurtej736l <gurtej154@gmail.com>
2021-10-05 11:08:00 +1030

    creating functioning js questions

--- script.js
+++ script.js
@@ -23,3 +23,60 @@ continue_btn.onclick = ()=> {
     info_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
 }
+
+//When a probable answer is clicked, a alert is given on weather the answer is correct or incorrect
+
+var letQuestions = [{
+        question: "What are the JavaScript data types?",
+        answers: {
+            a:"Number",
+            b:"String",
+            c:"Boolean",
+            d:"Object",
+            e:"Undefined",
+            f:"All the above"
+        }, 
+        correctAnswer: "All the above"
+    }, 
+    {
+        question: "The condition in an if or else statement is enclosed within?",
+        answers: {
+            a:"Quotes",
+            b:"Curly brackets",
+            c:"Parentheses",
+            d:"Square brackets"
+        },
+        correctAnswer: "Parentheses"
+    },
+    {
+        question: "What tag does JavaScript begin with?",
+        answers: {
+            a:"Script tag",
+            b:"JavaScript tag",
+            c:"Language tag",
+            d:"None of the above"
+        },
+        correctAnswer: "Script tag"
+    },
+    { 
+        question: "JavaScript is a ______ programming language?",
+        answers: {
+            a:"Client",
+            b:"Serverside",
+            c:"Java",
+            d:"None"
+        },
+        correctAnswer: "Server-side"
+    },
+    {
+        question: "Which of the following will write the message 'Hello World!' in an alert box?",
+        answers: {
+            a:"alert('Hello World!');",
+            b:"alert(HelloWorld!);",
+            c:"msgAlert('Hello World!');",
+            d:"alert('Hello World!');"
+        },
+        correctAnswer: "alert('Hello World');"
+    }]
+
+    
